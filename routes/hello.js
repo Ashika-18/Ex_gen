0c666e71637f745329004b5c97c90783a6130f82
Ashika <ashibasama@gmail.com>
2024-01-25 18:45:30 +0900

    次はshow.ejs

--- routes/hello.js
+++ routes/hello.js
@@ -12,17 +12,38 @@ router.get('/', (req, res, next) => {
         var rows = "";
         db.each("select * from mydb", (err, row) => {
             if (!err) {
-                rows += "<tr><th>" + row.id + "</th><td>" + row.name + "</td></tr>"
+                rows += "<tr><th>" + row.id + "</th><td>" + row.name + "</td></tr>";
             }
         }, (err, count) => {
             if (!err) {
                 var data = {
-                    title: 'Hello!',
+                    title: "Hello",
                     content: rows
                 };
-                res.render('hello/index', data);
-            }
+                res.render('hello', data);
+            }   
         });
     });
 });
+
+//GETとPOSTアクセスの処理
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'Hello/Add',
+        content: '新しいレコードを入力:'
+    }
+    res.render('hello/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    db.serialize(() => {
+        db.run('insert into mydb (name, mail, age) values (?, ?, ?)',
+        nm, ml, ag);
+    });
+    res.redirect('/hello');
+});
+
 module.exports = router;
