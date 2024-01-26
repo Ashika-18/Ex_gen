aeb726663eeddc09b4a063471eaba97e77c8f222
Ashika <ashibasama@gmail.com>
2024-01-26 18:03:35 +0900

    検索条件完了

--- routes/hello.js
+++ routes/hello.js
@@ -64,4 +64,96 @@ router.get('/show', (req, res, next) => {
     });
 });
 
+//editアクセスの処理
+router.get('/edit', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydb where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'hello/edit',
+                    content: 'id =' + id + 'のレコードを編集',
+                    mydata: row
+                }
+                res.render('hello/edit', data);
+            }
+        });
+    });
+});
+
+router.post('/edit', (req, res, next) => {
+    const id = req.body.id;
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    const q = "update mydb set name = ?, mail = ?, age = ? where id = ?";
+    db.serialize(() => {
+        db.run(q, nm, ml, ag, id);
+    });
+    res.redirect('/hello');
+});
+
+//delete処理
+router.get('/delete', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydb where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/Delete',
+                    content: 'id = ' + id + 'のレコードを削除します:',
+                    mydata: row
+                }
+                res.render('hello/delete', data);
+            }
+        });
+    });
+});
+
+router.post('/delete', (req, res, next) => {
+    const id = req.body.id;
+    db.serialize(() => {
+        const q = "delete from mydb where id = ?";
+        db.run(q, id);
+    });
+    res.redirect('/hello');
+});
+
+//find処理
+router.get('/find', (req, res, next) => {
+    db.serialize(() => {
+        db.all("select * from mydb ", (err, rows) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/Find',
+                    find: '',
+                    content: '検索条件を入力して下さい。',
+                    mydata: rows
+                };
+                res.render('hello/find', data);
+            }
+        });
+    });
+});
+
+router.post('/find', (req, res, next) => {
+    var find = req.body.find;
+    db.serialize(() => {
+        var q = "select * from mydb where" ;
+        db.all(q + find, [], (err, rows) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/Find',
+                    find: find,
+                    content: '検索条件' + find,
+                    mydata: rows
+                }
+                res.render('/hello/find', data);
+            }
+        });
+    });
+});
+
 module.exports = router;
