3a3bf6a3cc29993996416252183b6487de810be5
Ashika <ashibasama@gmail.com>
2024-01-28 18:17:39 +0900

    削除処理完了

--- routes/users.js
+++ routes/users.js
@@ -99,4 +99,26 @@ router.post('/edit', (req, res, next) => {
   });
 });
 
+//削除delete処理
+router.get('/delete/:id', (req, res, next) => {
+  const id = req.params.id;
+  prisma.user.findUnique(
+    { where: { id: +id }}
+  ).then(usr => {
+    const data = {
+      title: 'Users/Delete',
+      user: usr
+    };
+    res.render('users/delete', data);
+  });
+});
+
+router.post('/delete', (req, res, next) => {
+  prisma.User.delete({
+    where: { id: +req.body.id}
+  }).then(() => {
+    res.redirect('/users');
+  });
+});
+
 module.exports = router;
