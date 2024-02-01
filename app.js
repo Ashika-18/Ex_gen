08002f70e337a8743f8af96a365389128121f9b8
Ashika <ashibasama@gmail.com>
2024-02-01 18:41:45 +0900

    次はboardsのindex

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var boardsRouter = require('./routes/boards');
 
 const session = require('express-session');
 
@@ -16,6 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/boards', boardsRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
