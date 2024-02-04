57bad6150bb08aead443dfd955d2b3342964e842
Ashika <ashibasama@gmail.com>
2024-02-04 20:21:15 +0900

    dbをrender内のurlに変更

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Expressかな' });
+  res.render('index', { title: 'Expressです' });
 });
 
 module.exports = router;
