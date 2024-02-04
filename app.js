e2e733a0731a47f5b038aaf8775379839e873a33
Ashika <ashibasama@gmail.com>
2024-02-04 21:53:52 +0900

    hello削除

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var helloRouter = require('./routes/hello');
+
 var boardsRouter = require('./routes/boards');
 
 const session = require('express-session');
@@ -33,7 +33,7 @@ app.use(session(session_opt));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
+
 app.use('/boards', boardsRouter);
 
 // catch 404 and forward to error handler
