01795fd575f4a2495cc99d5734ba848c2decbebb
Ashika <ashibasama@gmail.com>
2024-02-26 18:04:25 +0900

    alertウィンドウ設定

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,10 @@
-// function confirmDelete(messageId) {
-//     if (confirm('このメッセージを削除しますか？')) {
-//         // 確認ダイアログでOKをクリックした場合、削除リクエストを送信する
-//         window.location.href = '/boards/delete/' + messageId;
-//     }
-// }
+function confirmDelete(messageId) {
+    if (confirm('このメッセージを削除しますか？')) {
+        // 確認ダイアログでOKをクリックした場合の処理
+        window.location.href = '/boards/delete/' + messageId;
+    } else {
+        // キャンセルされた場合の処理
+        alert('削除がキャンセルされました。');
+    }
+    console.log('読み込めてるよ😜')
+}
